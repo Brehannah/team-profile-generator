4d7b7327092670c06c38b7395ed2961febcedf34
Brehannah <57406410+Brehannah@users.noreply.github.com>
2020-06-11 23:10:49 -0400

    recent update

--- intern.test.js
+++ intern.test.js
@@ -3,18 +3,18 @@ const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "College";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Best", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "College");
+  const e = new Intern("Best", 1, "test@test.com", "College");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
   const testValue = "College";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Best", 1, "test@test.com", testValue);
   expect(e.getSchool()).toBe(testValue);
 });
